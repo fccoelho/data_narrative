36fa4ea01a8394e43edfe35b14b7c888c9c5ca37
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-09-14 14:41:07 -0300

    disabled mypy from pre-commit

--- data_narrative/narratives.py
+++ data_narrative/narratives.py
@@ -1,21 +1,31 @@
-import duckdb
 import jinja2
+from regdbot.brain import dbtools as dbt
+from base_agent.llminterface import LangModel
+from typing import List
+
 
 class StructuralNarrative:
-    '''
+    """
     A class to generate a structural narrative for a database table
-    '''
-    def __init__(self, conn: duckdb.DuckDBPyConnection):
-        self.conn = conn
+    """
+
+    def __init__(self, dburl: str, llm: str = "llama3.1"):
+        self.dburl = dburl
+        model = LangModel(llm)
+        self.database = dbt.Database(dburl, model)
 
     def generate(self, table: str) -> str:
-        query = f"SELECT * FROM {table}"
-        result = self.conn.execute(query)
-        columns = [col[0] for col in result.description]
-        rows = result.fetchall()
+        sample = self.database._get_sample_rows(table)
+        description = self.database.get_table_description(table)
+        columns = [
+            col.split(":")[1]
+            for col in description.split("\n")
+            if col.startswith("column")
+        ]
+        rows = sample
         return self.render(table, columns, rows)
 
-    def render(self, table: str, columns: list, rows: list) -> str:
+    def render(self, table: str, columns: List, rows: List) -> str:
         template = jinja2.Template(
             """
             Table: {{ table }}
--- tests/test_narratives.py
+++ tests/test_narratives.py
@@ -1,14 +1,13 @@
-import pytest
-import duckdb
 from data_narrative.narratives import StructuralNarrative
 
-con = duckdb.connect(database=":memory:")
-con.execute('CREATE TABLE  dengue as select * from "tests/test_datasets/SE_dengue.csv.gz"')
+
+dburl = "CSV:test_datasets/SE_dengue.csv.gz"
+
 
 def test_struct_narrative():
-    SN = StructuralNarrative(con)
-    narrative = SN.generate("dengue")
-    assert "Table: dengue" in narrative
-    assert "Columns: data_iniSE, SE" in narrative
-
-
+    SN = StructuralNarrative(dburl)
+    tables = SN.database.tables
+    narrative = SN.generate(tables[0])
+    assert f"Table: {tables[0]}" in narrative
+    assert "Columns: data_iniSE" in narrative
+    assert "geocodigo" in narrative
